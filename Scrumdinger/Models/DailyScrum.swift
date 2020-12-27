59008386df321dd1002c735a87bac0a548eb1bab
David Bailey <davidbailey00@outlook.com>
2020-12-27 14:17:09 +0000

    Add CardView + DailyScrum model + color files

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,25 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by David Bailey on 27/12/2020.
+//
+
+import SwiftUI
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+}
+
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Jess", "Olga", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
+            DailyScrum(title: "Web Dev", attendees: ["David", "Jennifer", "James", "Bolaji"], lengthInMinutes: 1, color: Color("Web Dev"))
+        ]
+    }
+}
