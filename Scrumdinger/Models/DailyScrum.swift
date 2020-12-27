83b05898301ca93c54e7fc4999ffae17ddbdee6a
David Bailey <davidbailey00@outlook.com>
2020-12-27 14:25:20 +0000

    add trailing comma to daily scrum data
    trailing commas reduce unnecessary git diff lines

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -19,7 +19,7 @@ extension DailyScrum {
         [
             DailyScrum(title: "Design", attendees: ["Jess", "Olga", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
             DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
-            DailyScrum(title: "Web Dev", attendees: ["David", "Jennifer", "James", "Bolaji"], lengthInMinutes: 1, color: Color("Web Dev"))
+            DailyScrum(title: "Web Dev", attendees: ["David", "Jennifer", "James", "Bolaji"], lengthInMinutes: 1, color: Color("Web Dev")),
         ]
     }
 }
