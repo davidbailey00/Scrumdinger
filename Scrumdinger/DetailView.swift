c0e59e1d46a388315f99dfc4896c75aca21d84b8
David Bailey <davidbailey00@outlook.com>
2020-12-30 13:57:05 +0000

    Add DetailView and link to it from ScrumsView

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,53 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by David Bailey on 30/12/2020.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "circle.fill")
+                        .foregroundColor(scrum.color)
+                        .accessibilityElement(children: .ignore)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) {attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.data[0])
+        }
+    }
+}
