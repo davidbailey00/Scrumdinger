9bb15521f21d419763ab2b884b4ebb73c4b9ee65
David Bailey <davidbailey00@outlook.com>
2021-01-07 22:32:18 +0000

    Extract meeting footer into new view; create binding to speaker data + skip action

---
+++ Scrumdinger/MeetingFooterView.swift
@@ -0,0 +1,42 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by David Bailey on 07/01/2021.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    @Binding var speakers: [ScrumTimer.Speaker]
+    var skipAction: () -> Void
+    private var speakerNumber: Int? {
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
+        return index + 1
+    }
+    private var isLastSpeaker: Bool {
+        return speakers.dropLast().allSatisfy { $0.isCompleted }
+    }
+    private var speakerText: String {
+        guard let speakerNumber = speakerNumber else { return "No more speakers" }
+        return "Speaker \(speakerNumber) of \(speakers.count)"
+    }
+    var body: some View {
+        HStack {
+            Text(speakerText)
+            Spacer()
+            Button(action: skipAction) {
+                Image(systemName: "forward.fill")
+            }
+            .accessibilityLabel(Text("Next speaker"))
+        }
+    }
+}
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var speakers = DailyScrum.data[0].timer.speakers
+    static var previews: some View {
+        MeetingFooterView(speakers: .constant(speakers), skipAction: {})
+            .previewLayout(.sizeThatFits)
+    }
+}
